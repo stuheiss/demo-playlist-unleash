b3cc4359a23eb976177d9a68c13e0f70f6decd10
Stu Heiss <sheiss@highlandsolutions.com>
2018-03-26 08:30:35 -0500

    checkpoint

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import ULClient from './unleash-js.js'
 const isNewer = (a, b) => Date.parse(a.added) < Date.parse(b.added)
 
 const features = [
-  // {
-  //   flag: 'test2',
-  //   strategies: [{ perUser: ['test2user', 'test2user2', 'test3user'] }]
-  // },
-  // { flag: 'test2' },
+  { flag: 'test2', userId: 'test2user' },
+  { flag: 'test2', userId: 'test2user2' },
+  { flag: 'test2', userId: 'test3user' },
   { flag: 'test3.colorScheme' },
   { flag: 'test3.powerUser' },
   { flag: 'test3.timeSorting' }
@@ -30,141 +28,55 @@ class App extends Component {
       ]
     }
   }
+
   componentDidMount() {
     this.ulclient = ULClient.initialize()
     this.ulclient.on('ready', this.onUnleashReady.bind(this))
     this.ulclient.on('change', this.onUnleashChange.bind(this))
   }
+
   onUnleashReady(event) {
-    console.log('onUnleashReady', event)
-    let pending = []
-    const features = this.state.features
-    features.map(feature => {
-      const enabled = this.ulclient.variation(feature.flag, feature.userId)
-      pending.push(enabled)
-      feature.enabled = enabled
-    })
-    Promise.all(pending).then(values => {
-      console.log('wakeup', values)
-      this.setState(features)
-    })
-    return null
+    //console.log('onUnleashReady', event)
+    this.unleashUpdate(event)
   }
 
-  updateFeatureState(flag, userId) {
-    console.log('updateFeatureState', flag, userId)
-    let pending = []
-    if (typeof flag === 'undefined') {
-      let features = this.state.features
-      features.map(feature => {
-        if (typeof feature.strategies === 'undefined') {
-          let enabled = this.ulclient.variation(feature.flag)
-          pending.push(enabled)
-          feature.enabled = enabled
-        } else {
-          feature.strategies.map(strategy => {
-            if (typeof strategy.perUser !== 'undefined') {
-              strategy.perUser.map(userId => {
-                let enabled = this.ulclient.variation(feature.flag, userId)
-                pending.push(enabled)
-                feature.enabled = enabled
-              })
-            }
-          })
-        }
-      })
-      Promise.all(pending).then(() => {
-        this.setState({ features: features })
-        console.log('UPDall', features)
-      })
-    } else {
-      //console.log('updateFS', flag, userId)
-      let enabled = this.ulclient.variation(flag, userId)
-      pending.push(enabled)
-      let features = this.state.features
-      let feature = features.filter(f => f.flag === flag)[0]
-      feature.enabled = enabled
-      let newFeatures = features.map(f => (f.flag === flag ? feature : f))
-      Promise.all(pending).then(() => {
-        this.setState({ features: newFeatures })
-        console.log('UPD1', features)
-      })
-    }
-  }
   onUnleashChange(event) {
-    console.log('unUnleashChange event', event)
-    let flag = event.flag
-    let userId = event.userId
+    //console.log('unUnleashChange event', event)
+    this.unleashUpdate(event)
+  }
+
+  unleashUpdate(event) {
     let pending = []
-    const features = this.state.features
+    const features = this.state.features.slice()
     features.map(feature => {
-      if (feature.flag === flag && feature.userId === userId) {
-        const enabled = this.ulclient.variation(feature.flag, feature.userId)
-        pending.push(enabled)
-        feature.enabled = enabled
+      if (
+        event === 'wakeup' ||
+        (feature.flag === event.flag && feature.userId === event.userId)
+      ) {
+        pending.push(this.ulclient.variation(feature.flag, feature.userId))
+      } else {
+        pending.push(feature.enabled)
       }
+      return null
     })
     Promise.all(pending).then(values => {
-      console.log('change', values)
-      this.setState(features)
-    })
-    return null
-
-    /*  
-    if (typeof event === 'undefined') {
-      console.log('unUnleashChange event not defined')
-    } else if (event === 'wakeup') {
-      this.updateFeatureState() // updates all defined features
-    } else {
-      this.updateFeatureState(event.flag, event.userId)
-    }
-
-    if (false) {
-      this.state.features.map(feature => {
-        //console.log('mapFeatures', feature.flag)
-        if (!feature.hasOwnProperty('strategies')) {
-          let value = this.ulclient.variation(feature.flag)
-          if (typeof value !== 'undefined') {
-            value.then(enabled => {
-              //console.log('feature', feature.flag, enabled)
-            })
-          }
-        } else {
-          feature.strategies.map(strategy => {
-            if (strategy.hasOwnProperty('perUser')) {
-              strategy.perUser.map(userId => {
-                let value = this.ulclient.variation(feature.flag, userId)
-                if (typeof value !== 'undefined') {
-                  value.then(enabled => {
-                    //console.log('feature', feature.flag, userId, enabled)
-                  })
-                }
-                return null
-              })
-            }
-            return null
-          })
-        }
+      features.map((feature, i) => {
+        feature.enabled = values[i]
         return null
       })
-    }
-    */
+      this.setState({ features: features })
+    })
   }
 
   getFeature(flag, userId) {
     let feature = this.state.features.filter(
       f => f.flag === flag && f.userId === userId
     )[0]
-    console.log('getFeature', feature)
-    return typeof feature === 'undefined' ? false : feature.enabled
-  }
-
-  async foo(flag, userId) {
-    let feature = this.state.features.filter(
-      f => f.flag === flag && f.userId === userId
-    )[0]
-    console.log('getFeature', feature)
-    return (await typeof feature) === 'undefined' ? false : feature.enabled
+    if (typeof feature === 'undefined') return undefined
+    return typeof feature !== 'undefined' &&
+      typeof feature.enabled === 'boolean'
+      ? feature.enabled
+      : undefined
   }
 
   render() {
@@ -172,20 +84,20 @@ class App extends Component {
     //   return <div className="App">Loading....</div>
     // }
 
-    let sorter = undefined
-    let v = this.getFeature('test3.timeSorting')
-    let v2 = v === true ? true : false
-    console.log('v', v, 'v2', v2)
-    let xsorter = v ? isNewer : undefined
-    sorter = xsorter
-    console.log('sorter', sorter, 'v', v, 'xsorter', xsorter)
-    let v3 = this.foo('test3.timeSorting')
-    console.log('v3', v3 ? 'v3true' : 'v3false')
-    // if (this.state.featureFlags.defaultSortingIsAdded) {
-    //   sorter = isNewer
-    // } else {
-    //   sorter = undefined
-    // }
+    let sorter
+    if (this.state.selectedSortOrder) {
+      if (this.state.selectedSortOrder === 'added') {
+        sorter = isNewer
+      } else if (this.state.selectedSortOrder === 'natural') {
+        sorter = undefined
+      }
+    } else {
+      if (this.getFeature('test3.timeSorting')) {
+        sorter = isNewer
+      } else {
+        sorter = undefined
+      }
+    }
 
     return (
       <div className="App">
